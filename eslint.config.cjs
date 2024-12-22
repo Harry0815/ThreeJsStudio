97ce211544f20d9a0568d3ff108377f01b251029
Harry0815 <hschmidt@edv-schmidt.com>
2024-12-23 00:14:07 +0100

    feat: eslint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -10,6 +10,19 @@ module.exports = [
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
+    },
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {
       '@nx/enforce-module-boundaries': [
         'error',
         {
@@ -23,18 +36,50 @@ module.exports = [
           ],
         },
       ],
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-confusing-void-expression": "off",
+      "deprecation/deprecation": "off",
+      "import/no-default-export": "off",
+      "@typescript-eslint/unbound-method": [
+        "error",
+        {
+          "ignoreStatic": true
+        }
+      ],
+      "@typescript-eslint/prefer-readonly": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_"
+        }
+      ],
+      "@typescript-eslint/prefer-nullish-coalescing": [
+        "error",
+        {
+          "ignorePrimitives": {
+            "string": true,
+            "boolean": true,
+            "number": true,
+            "bigint": true
+          }
+        }
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          "paths": ["rxjs/Rx"],
+          "patterns": ["**/../dist", "**/../src"]
+        }
+      ],
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/restrict-plus-operands": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "no-extra-semi": "off"
     },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
   },
 ];
